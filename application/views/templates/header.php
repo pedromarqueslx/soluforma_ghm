3c315fec44caef1ec144fd3408cc817149e7d86d
Pedro Marques <pedromarqueslx@gmail.com>
2020-05-21 15:16:42 +0100

    Removed Github security alert files

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,35 +13,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="shortcut icon" href="<?php echo base_url("/assets/img/favicon.ico"); ?>">
 
-<!-- For Bootstrap CSS files: -->
-<!--
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" href="https://v4-alpha.getbootstrap.com/dist/css/bootstrap.min.css">
--->
-<!-- OFFLINE -->
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bootstrap-4.0.0/dist/css/bootstrap.css"> 
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bootstrap-4.0.0/dist/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bootstrap-4.0.0/bootstrap.min.css">
 
-<!-- For Bootstrap JS files: -->
-<!--
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
--->
-<!-- OFFLINE -->
-<script src="<?php echo base_url(); ?>assets/bootstrap-4.0.0/dist/js/jquery-3.2.1.slim.min.js"></script> 
+<script src="<?php echo base_url(); ?>assets/bootstrap-4.0.0/dist/js/jquery-3.2.1.slim.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/bootstrap-4.0.0/dist/js/popper.min.js"></script> 
 <script src="<?php echo base_url(); ?>assets/bootstrap-4.0.0/dist/js/bootstrap.min.js"></script> 
 
-
-
-<!-- LOAD DATATABLES.NET -->
-<!--
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.4.1/css/buttons.dataTables.min.css">
--->
-
-<!-- OFFLINE -->
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/DataTables/jquery.dataTables.min.css">
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/DataTables/buttons.dataTables.min.css">
 
