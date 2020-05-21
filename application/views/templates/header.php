4ded4ae1514f6aba11ad06cc32ad355d350a8398
Pedro Marques <pedromarqueslx@gmail.com>
2020-05-21 23:48:36 +0100

    Optimize assets loading

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,12 +13,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="shortcut icon" href="<?php echo base_url("/assets/img/favicon.ico"); ?>">
 
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bootstrap-4.0.0/dist/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bootstrap-4.0.0/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bootstrap/bootstrap.min.css">
 
-<script src="<?php echo base_url(); ?>assets/bootstrap-4.0.0/dist/js/jquery-3.2.1.slim.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/bootstrap-4.0.0/dist/js/popper.min.js"></script> 
-<script src="<?php echo base_url(); ?>assets/bootstrap-4.0.0/dist/js/bootstrap.min.js"></script> 
+<script src="<?php echo base_url(); ?>assets/bootstrap/js/jquery-3.2.1.slim.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/bootstrap/js/popper.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
 
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/DataTables/jquery.dataTables.min.css">
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/DataTables/buttons.dataTables.min.css">
