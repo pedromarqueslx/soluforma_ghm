f2100f45713e692a42247a3e093475284bf7ffe0
Pedro Marques <pedromarqueslx@gmail.com>
2021-04-22 21:01:46 +0100

    New Main Menu

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -3,11 +3,11 @@
     $segment_1 = $this->uri->segment(1);
     $segment = $this->uri->segment(2);
     $segment_3 = $this->uri->segment(3);
-    $user[''] = 0;
-    $servicos2021[''] = 0;
+    /*$user[''] = 0;*/
+/*  $servicos2021[''] = 0;
     $servicos2020[''] = 0;
     $servicos2019[''] = 0;
-    $servicos[''] = 0;
+    $servicos[''] = 0;*/
 ?>
 
 <div class="container-fluid bg-light">
@@ -24,63 +24,65 @@
     </div>
 </div>
 
-
-<div class="container">
-    <div class="row">
-        <?php
+    <?php
         if ($user['user_profile'] == 'Administrator') {
-            echo '<div class="col-md-12">';
-            echo '<div class="table-responsive">';
-            echo '<table class="table table-striped">';
-            echo '<thead>';
-            echo '<tr>';
-            echo '<th><h2>Últimas Formações de 2021</h2></th>';
-            echo '</tr>';
-            echo '</thead>';
-            echo '<tbody>';
-            foreach ($servicos2021 as $servicos_item) {
-                echo '<tr>';
-                echo '<td>';
-                echo '<a href="';
-                echo site_url('servicos2021/edit/' . $servicos_item['id']);
-                echo '">';
-                echo $servicos_item['title'];
-                echo '</a></td>';
-                echo '<td>';
-                echo '<a href="';
-                echo site_url('servicos2021/edit/' . $servicos_item['id']);
-                echo '">';
-                echo $servicos_item['area_servicos'];
-                echo '</a></td>';
-                echo '<td>';
-                echo '<a href="';
-                echo site_url('servicos2021/edit/' . $servicos_item['id']);
-                echo '">';
-                echo $servicos_item['horas_servicos'];
-                echo '</a></td>';
-                echo '<td>';
-                echo '<a href="';
-                echo site_url('servicos2021/edit/' . $servicos_item['id']);
-                echo '">';
-                echo $servicos_item['formadores_servicos'];
-                echo '</a></td>';
-                echo '<td>';
-                echo '<a href="';
-                echo site_url('servicos2021/edit/' . $servicos_item['id']);
-                echo '">';
-                echo $servicos_item['data_servicos'];
-                echo '</a></td>';
-                echo '</tr>';
-            }
-            echo '</tbody>';
-            echo '</table>';
-            echo '</div>';
-            echo '</div>';
-        }
+    ?>
+    <div class="container">
+        <div class="row">
+    <?php
+    echo '<div class="col-md-12">';
+    echo '<div class="table-responsive">';
+    echo '<table class="table table-striped">';
+    echo '<thead>';
+    echo '<tr>';
+    echo '<th><h2>Últimas Formações de 2021</h2></th>';
+    echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+    foreach ($servicos2021 as $servicos_item) {
+        echo '<tr>';
+        echo '<td>';
+        echo '<a href="';
+        echo site_url('servicos2021/edit/' . $servicos_item['id']);
+        echo '">';
+        echo $servicos_item['title'];
+        echo '</a></td>';
+        echo '<td>';
+        echo '<a href="';
+        echo site_url('servicos2021/edit/' . $servicos_item['id']);
+        echo '">';
+        echo $servicos_item['area_servicos'];
+        echo '</a></td>';
+        echo '<td>';
+        echo '<a href="';
+        echo site_url('servicos2021/edit/' . $servicos_item['id']);
+        echo '">';
+        echo $servicos_item['horas_servicos'];
+        echo '</a></td>';
+        echo '<td>';
+        echo '<a href="';
+        echo site_url('servicos2021/edit/' . $servicos_item['id']);
+        echo '">';
+        echo $servicos_item['formadores_servicos'];
+        echo '</a></td>';
+        echo '<td>';
+        echo '<a href="';
+        echo site_url('servicos2021/edit/' . $servicos_item['id']);
+        echo '">';
+        echo $servicos_item['data_servicos'];
+        echo '</a></td>';
+        echo '</tr>';
+    }
+    echo '</tbody>';
+    echo '</table>';
+    echo '</div>';
+    echo '</div>';
         ?>
+        </div>
     </div>
-</div>
-
+    <?php
+        }
+    ?>
 
 <div class="container">
     <div class="row">
@@ -195,11 +197,8 @@
 </div>
 
 <div class="container">
-
     <div class="row">
-
         <?php
-
         if ($user['user_profile'] == 'Administrator') {
             echo '<div class="col-12">';
             echo '<div class="table-responsive">';
@@ -258,4 +257,3 @@
     </div>
 
 </div>
-
--- application/views/users/login.php
+++ application/views/users/login.php
@@ -15,64 +15,57 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="<?php echo base_url("/assets/css/styles.css"); ?>">
         <style type="text/css">
-            body {
-                background: url("<?php echo base_url("/assets/img/07_background.jpg"); ?>") no-repeat center center fixed;
-                -webkit-background-size: cover;
-                -moz-background-size: cover;
-                -o-background-size: cover;
-                background-size: cover;
-            }
+        body {
+            background: url("<?php echo base_url("/assets/img/07_background.jpg"); ?>") no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
         </style>
     </head>
-
     <body>
-
     <div class="container">
-
-        <div class="row mt-5">
+        <div class="row">
             <div class="col text-center">
                 <a href="<?php echo site_url(); ?>">
-                    <img class="m-3 mx-auto" src="<?php echo base_url("/assets/img/logosoluforma.svg"); ?>" style="height:80px">
+                    <img class="m-2 mx-auto" src="<?php echo base_url("/assets/img/logosoluforma.svg"); ?>" style="height:85px">
                 </a>
+                <h1 class="text-center" style="color: black">Gestão de Histórico de Multas</h1>
             </div>
         </div>
-
         <div class="row justify-content-center">
-            <div class="col-md-6 mx-auto">
+            <div class="col-md-5 mx-auto">
                 <div class="card">
                     <div class="card-body p-5">
-                        <form class="form-signin" action="" method="post">
-                            <?php
-                            if(!empty($error_msg)) {
-                                echo '<p class="small">'.$error_msg. '</p>';
-                            }
-                            ?>
-                            <div class="mt-2 mb-2">
-                                <label for="email">Endereço de E-mail</label>
-                                <input type="email" name="email" class="form-control" placeholder="E-mail" required autocomplete="email" autofocus>
-                                <?php echo form_error('email','<span class="help-block">','</span>'); ?>
-                            </div>
-                            <div class="mt-3 mb-2">
-                                <label for="password">Senha de Acesso</label>
-                                <input type="password" name="password" class="form-control" placeholder="" required>
-                                <div class="small"><?php echo form_error('password'); ?></div>
-                            </div>
-                            <input type="text" name="login_date" value="<?php echo date("Y-m-j"); ?>" id="inputPassword" hidden/>
-                            <button class="btn btn-lg btn-primary btn-block mt-4" type="submit" name="loginSubmit" value="Submit">Iniciar Sessão</button>
-                        </form>
+                    <form class="form-signin" action="" method="post">
+                        <?php
+                        if(!empty($error_msg)) {
+                            echo '<p class="small">'.$error_msg. '</p>';
+                        }
+                        ?>
+                        <div class="mt-2 mb-2">
+                            <label for="email">Endereço de E-mail</label>
+                            <input type="email" name="email" class="form-control" placeholder="E-mail" required autocomplete="email" autofocus>
+                            <?php echo form_error('email','<span class="help-block">','</span>'); ?>
+                        </div>
+                        <div class="mt-3 mb-2">
+                            <label for="password">Senha de Acesso</label>
+                            <input type="password" name="password" class="form-control" placeholder="" required>
+                            <div class="small"><?php echo form_error('password'); ?></div>
+                        </div>
+                        <input type="text" name="login_date" value="<?php echo date("Y-m-j"); ?>" id="inputPassword" hidden/>
+                        <button class="btn btn-lg btn-primary btn-block mt-4" type="submit" name="loginSubmit" value="Submit">Iniciar Sessão</button>
+                    </form>
                     </div>
                 </div>
             </div>
         </div>
-
         <div class="row m-5 justify-content-center">
             <div class="col text-center">
                 <p class="text-muted small">Soluforma, todos os direitos reservados &copy; 2018</p>
             </div>
         </div>
-
     </div>
-
     </body>
-
 </html>
